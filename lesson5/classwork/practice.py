4ef355f2ea2fa1ccd793fee8f43dfd951f32b515
LayaRaghuGH <layaraghu14@gmail.com>
2026-07-12 22:57:12 +0000

    Finished lesson 5

--- lesson5/classwork/practice.py
+++ lesson5/classwork/practice.py
@@ -1,27 +1,41 @@
 # Problem 1
 # Search the list and print whether "banana" is found.
 fruits = ["apple", "orange", "banana", "grape"]
-
+if "banana" in fruits: 
+    print("Found banana")
+else:
+    print("No bananas found")
 
 
 # Problem 2
 # Count how many numbers are greater than 10 in the list.
 numbers = [4, 18, 2, 30, 7, 12]
-
-
+counter = 0
+for i in range(len(numbers)):  
+    item = numbers[i]
+    if item > 10: 
+        counter = counter + 1
+print(counter, "numbers above 10")
 
 # Problem 3
 # Find and print the total sum of all the numbers in the list.
 numbers = [4, 11, 22, -6, 3]
-
+total = sum(numbers)
+print("The sum of these numbers is:", total)
 
 
 # Problem 4
 # Find and print the biggest number in the list.
 numbers = [-9, 17, 5, -3, 0]
-
-
+biggest_item = max(numbers)
+print("The biggest numbers is:", biggest_item)
 
 # Problem 5
 # Find and print the sum of only the even numbers in the list. 
 numbers = [8, 3, 15, 22, 11, 6]
+counter = 0
+
+for i in numbers:
+    if i % 2 == 0:
+        counter += i
+print("The sum of these numbers is:", counter)
